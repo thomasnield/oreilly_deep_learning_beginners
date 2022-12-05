be3b4c8885d3daec11ad16c394e5484782cad1fc
Thomas Nield <thomasnield@live.com>
2022-12-05 07:18:58 -0600

    Create exercise_3_answer.py

---
+++ exercises/exercise_3_answer.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+all_data = pd.read_csv("https://bit.ly/3wlFsb4")
+
+# Extract the input columns, scale down by 255
+X = (all_data.iloc[:, 0:3].values / 1000.0)
+Y = all_data.iloc[:, -1].values
+
+# Build neural network with weights and biases
+# with random initialization
+w_hidden = np.random.rand(3, 3)
+w_output = np.random.rand(1, 3)
+
+b_hidden = np.random.rand(3, 1)
+b_output = np.random.rand(1, 1)
+
+# Activation functions
+relu = lambda x: np.maximum(x, 0)
+logistic = lambda x: 1 / (1 + np.exp(-x))
+
+# Runs inputs through the neural network to get predicted outputs
+def forward_prop(X):
+    Z1 = w_hidden @ X + b_hidden
+    A1 = relu(Z1)
+    Z2 = w_output @ A1 + b_output
+    A2 = logistic(Z2)
+    return Z1, A1, Z2, A2
+
+# Calculate accuracy
+test_predictions = forward_prop(X.transpose())[3]  # grab only A2
+test_comparisons = np.equal((test_predictions >= .5).flatten().astype(int), Y)
+accuracy = sum(test_comparisons.astype(int) / X.shape[0])
+print("ACCURACY: ", accuracy)
+
