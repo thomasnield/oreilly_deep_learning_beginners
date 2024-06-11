442430da165ea11c4376af88679f9542dcf6d8d1
thomasnield <thomasnield@live.com>
2024-06-11 13:18:29 -0500

    :

---
+++ exercises/exercise_1_answer.py
@@ -0,0 +1,44 @@
+"""
+In the Python code below, replace the question marks "?" 
+with the proper code to perform a neural network prediction 
+on a maintenance dataset on whether a part needs replacement (1) or not (0).
+
+Use 3 nodes in the hidden layer, and ReLU as the activation function.
+
+Experiment with learning rate and iterations to optimize training. 
+"""
+
+import pandas as pd
+import tensorflow as tf
+
+df = pd.read_csv('https://bit.ly/3wlFsb4')
+
+# Extract input variables (all rows, all columns but last column)
+# Note we should do some linear scaling here
+X = df.values[:, :-1] / 1000.0
+
+# Extract output column (all rows, last column)
+Y = df.values[:, -1]
+
+# Separate training and testing data
+
+# declare the model 
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(3, activation='relu'),
+  tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+loss_fn = tf.keras.losses.MeanSquaredError()
+
+# compile the model 
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+# fit the model
+model.fit(X, Y, epochs=100, batch_size=32)
+
+
+# evaluate the model
+scores = model.evaluate(X, Y, verbose=0)
+print(f"Dataset Score: {scores[1]}")
